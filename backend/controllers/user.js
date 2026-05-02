26946fac62b9790fccbee490985ca8f0120a2443
Sampriti1 <sampritik100@gmail.com>
2026-05-02 16:37:31 +0530

    updated code

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -27,6 +27,7 @@ async function handleUserLogin(req, res) {
     res.cookie("token", token, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === 'production', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     });
 
     
--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(cors({
 }))
 
 app.use(cookieParser());
- 
+app.set('trust proxy', 1);
 app.use(express.json()); 
 app.use(checkForAuthentication);
 app.use(express.urlencoded({ extended: false}))
