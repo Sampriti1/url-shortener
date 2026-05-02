efae83bf7130217ffba45c206e57de55da779b2d
Sampriti1 <sampritik100@gmail.com>
2026-05-02 15:54:12 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectToMongoDB(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/short-url")
   .then(() => console.log("MongoDb connected"));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }))
 
